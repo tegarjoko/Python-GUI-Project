3101256a5117d11375249f05b955498974b683bd
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2021-11-16 17:47:09 +0700

    tambah form sebelum cek database

--- Final_Login.py
+++ Final_Login.py
@@ -15,25 +15,6 @@ class loginForm:
         self.username.place(x=200,y=200)
 
         Button(text='LOGIN',command=self.login_user).place(x=200,y=220)
-        Button(text='REGISTRASI',command=self.registerForm).place(x=250,y=220)
-
-    def userBaru(self):
-        self.userBaru = Toplevel(root)
-        infoBaru = self.namaUser
-        self.tambahUser = self.user.append(infoBaru)
-        if infoBaru in self.user and self.passrahasia == '2001':
-            Label(self.userBaru,text="Berhasil !").pack()
-        else:
-            Label(self.userBaru,text="Gagal!").pack()
-
-    def registerForm(self):
-        self.registerForm = Toplevel(root)
-        self.registerForm.title('REGISTRASI')
-        Label(self.registerForm,text="Masukan User Baru :").grid(row=0,column=0)
-        self.namaUser = Entry(self.registerForm,width=50).grid(row=1,column=0)
-        Label(self.registerForm,text="Password Admin :").grid(row=2,column=0)
-        self.passrahasia = Entry(self.registerForm,width=50).grid(row=3,column=0)
-        Button(self.registerForm,text='REGISTER',command=self.userBaru).grid(row=4,column=0)
 
     def login_user(self):
         if self.username.get() == self.user:
@@ -43,7 +24,7 @@ class loginForm:
             application = MenuUtama(newroot)
             newroot.mainloop()
         else:
-            self.message = Label(text = 'Username or Password incorrect. Try again!',fg = 'Red')
+            self.message = Label(text = 'Username Salah! Silahkan coba lagi.',fg = 'Red')
             self.message.place(x=200,y=250)
 
 if __name__ == '__main__':
--- Final_Menu.py
+++ Final_Menu.py
@@ -1,19 +1,72 @@
 from tkinter import *
+import sqlite3
+from tkcalendar import Calendar
+import datetime as datee
 
 class MenuUtama:
-    def __init__(self,root):
-        self.root = root
-        root.geometry('700x500')
-        root.title('Menu Utama')
-        bg = PhotoImage(file='Menu.png')
-        label1 = Label(root, image = bg)
-        label1.place(x = 0, y = 0)
 
-        #Tombol
-        Button(root, text = 'Halo').grid(row=0, column=0)
+    db_name = 'catering.db' 
+
+
+
+
+
+    def __init__(self,root):
+
+        self.root = root
+        root.title('Menu Utama')
+        bg1 = PhotoImage(file='Menu1.png')
+        label11 = Label(root, image = bg1)
+        label11.place(x = 0, y = 0)
+
+        #Function
+
+        paket = IntVar
+        x = datee.datetime.now().strftime(f"%d - %b - %Y")
+
+        cal = Calendar(root, selectmode = 'day')
+
+        cal.grid(row=2,rowspan = 7, column = 3)
+
+        def grad_date():
+            dateTtl.config(text ="Tanggal yang dipilih : "+ cal.get_date())       
+
+        #Tombol Data Baru
+        
+        nama = Label(root, text = 'Masukan Nama Anda ').grid(row=0, column=0,sticky=W)
+        namaEntry = Entry(root,width =30).grid(row=0, column=1,sticky=W)
+        paket = Label(root, text = 'Pilih Paket Anda ').grid(row=1, column=0,sticky=W)
+        r1 = Radiobutton(root,text='Pernikahan',variable=paket,value=1).grid(row=1, column=1,sticky=W)
+        r2 = Radiobutton(root,text='Ulang Tahun',variable=paket,value=2).grid(row=2, column=1,sticky=W)
+        r3 = Radiobutton(root,text='Hajatan',variable=paket,value=3).grid(row=3, column=1,sticky=W)
+        porsi = Label(root, text = 'Porsi ').grid(row=4, column=0,sticky=W)
+        porsiEntry = Entry(root,width =30).grid(row=4, column=1,sticky=W)
+        alamat = Label(root,text = 'Alamat').grid(row=5, column=0,sticky=W)
+        alamatEntry = Entry(root).grid(row=5,column=1,sticky=W)
+        tanggal = Label(root,text = 'Tanggal Pemesanan').grid(row=6, column=0,sticky=W)
+        tanggalEntry = Label(root,text = x ).grid(row=6, column=1,sticky=W)
+        gapMaker = Label(root,text ="     ").grid(row=0,rowspan=10,column=2)
+        ttglkirim = Label(root,text = 'Tanggal Kirim').grid(row=0, column=3)
+        saveBuxtton = Button(root,text='Pilih Tanggal',command=grad_date).grid(row=9,column=3)
+        dateTtl = Label(root,text ="Tanggal yang dipilih : ")
+        dateTtl.grid(row=1,column=3)
+
+        #Table View SQLite
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
     root = Tk()
+    root.title('Menu Utama')
+    bg = PhotoImage(file='Menu.png')
+    label1 = Label(root, image = bg)
+    label1.place(x = 0, y = 0)
     application = MenuUtama(root)
     root.mainloop()
