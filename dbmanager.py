7dacea055facaea0d13922db026df2941ba09a26
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2021-12-07 18:56:05 +0700

    final

--- dbmanager.py
+++ dbmanager.py
@@ -2,7 +2,7 @@ import sqlite3
 
 con = sqlite3.connect('catering.db')
 c = con.cursor()
-'''c.execute("""CREATE TABLE datapesananbarubaru (
+c.execute("""CREATE TABLE contoh (
     nama text,
     paket text,
     porsi integer,
@@ -11,12 +11,12 @@ c = con.cursor()
     tgl_krm text,
     total_harga integer,
     notlp integer
-)""")'''
+)""")
 
 
-#c.execute("INSERT INTO datapesananbarubaru VALUES ('Tegar','Ulang Tahun','100','Jl.Swadaya 2,Kec.Tambun Selatan,Kab.Bekasi','21/11/2021','25/11/2021','2000000','08122304844')")
+c.execute("INSERT INTO contoh VALUES ('Tegar','Ulang Tahun','100','Jl.Swadaya 2,Kec.Tambun Selatan,Kab.Bekasi','21/11/2021','25/11/2021','2000000','08122304844')")
 #c.execute("DELETE FROM datapesananbarubaru WHERE nama = 'Dosen 3'")
-c.execute("SELECT * FROM datapesananbarubaru")
+c.execute("SELECT * FROM contoh")
 print(c.fetchall())
 con.commit()
 con.close()
