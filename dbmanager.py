528c99937de9983d063efaa7cd11ef45c14db4b1
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2021-12-10 00:52:08 +0700

    nambah datakaryawan dan logadmin

--- dbmanager.py
+++ dbmanager.py
@@ -2,7 +2,7 @@ import sqlite3
 
 con = sqlite3.connect('catering.db')
 c = con.cursor()
-c.execute("""CREATE TABLE contoh (
+'''c.execute("""CREATE TABLE datapesananbarubaru (
     nama text,
     paket text,
     porsi integer,
@@ -11,12 +11,25 @@ c.execute("""CREATE TABLE contoh (
     tgl_krm text,
     total_harga integer,
     notlp integer
-)""")
+)""")'''
+
+'''c.execute("""CREATE TABLE datakaryawan1 (
+    id integer,
+    nama text,
+    password text,
+    notlp integer
+)""")'''
+
+'''c.execute("""CREATE TABLE logadmin (
+    nama text,
+    tglmasuk text,
+    tglkeluar text
+)""")'''
 
 
-c.execute("INSERT INTO contoh VALUES ('Tegar','Ulang Tahun','100','Jl.Swadaya 2,Kec.Tambun Selatan,Kab.Bekasi','21/11/2021','25/11/2021','2000000','08122304844')")
+#c.execute("INSERT INTO logadmin VALUES ('Tegar','tegar0987','08122304844')")
 #c.execute("DELETE FROM datapesananbarubaru WHERE nama = 'Dosen 3'")
-c.execute("SELECT * FROM contoh")
+c.execute("SELECT * FROM logadmin")
 print(c.fetchall())
 con.commit()
 con.close()
